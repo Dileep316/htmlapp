af63f49c4e2852cda148135b1b43361038344de2
root <root@ip-172-31-35-160.ap-south-1.compute.internal>
2023-06-13 07:36:18 +0000

    second

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { 'content-type': 'text/html' })
   fs.createReadStream('index.html').pipe(res)
 })
-server.listen(process.env.PORT || 8881)
+server.listen(process.env.PORT || 8888)
